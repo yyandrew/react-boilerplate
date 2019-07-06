214779c8da19b0cd7cd1a297ad5cd3af29a793b4
andrew yang <andrew@ekohe.com>
2019-07-06 09:41:30 +0800

    Add deleteNote event
    The flow is:
    
    |App|-onDelete->|Notes|-onDelete.bind(null, id)->|Note|
    |App|<-onDelete(id, e)-|Notes|<-onDelete(id, e)-|Note|

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -35,13 +35,20 @@ export default class App extends React.Component {
     })
   }
 
+  deleteNote = (id, e) => {
+    e.stopPropagation()
+    this.setState({
+      notes: this.state.notes.filter((note) => note.id !== id)
+    })
+  }
+
   render() {
     const { notes } = this.state
 
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} />
+        <Notes notes={notes} onDelete={this.deleteNote} />
       </div>
     )
   }
