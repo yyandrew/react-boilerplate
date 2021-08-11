f309cb47fadc5d10684865ff1e331253eb0027b6
andrew yang <qqerqqer@126.com>
2021-08-11 11:00:45 +0800

    Use reducer to manage complex state

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Puzzle from './Puzzle'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Puzzle</h1>
+      <Puzzle />
+    </div>
+  )
+}
+
+export default App
