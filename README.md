caba6b5f628ecd89c085aa91424a4f97ea4a3391
andrew yang <qqerqqer@126.com>
2021-08-15 22:08:32 +0800

    network calls to hooks

---
+++ README.md
@@ -0,0 +1,4 @@
+利用 hooks 请求网络数据。
+### 运行程序方法
+1. 运行命令 `node server.js` 启动服务端
+2. 运行 `yarn start` 启动客户端
---
+++ src/useMessages.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+
+function useMessages(forum) {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
+  useEffect(() => {
+    let didCancel = false
+    setError(null)
+    if (forum) {
+      ;(async () => {
+        try {
+          setLoading(true)
+          const response = await fetch(`/messages/${forum}`)
+          if (!response.ok) {
+            const text = await response.text()
+            throw new Error(`Unable to read messages for ${forum}: ${text}`)
+          }
+          const body = await response.json()
+          if (!didCancel) {
+            setData(body)
+          }
+        } catch (err) {
+          setError(err)
+        } finally {
+          setLoading(false)
+        }
+      })()
+    } else {
+      setData([])
+      setLoading(false)
+    }
+    return () => {
+      didCancel = true
+    }
+  }, [forum])
+
+  return { data, loading, error }
+}
+
+export default useMessages
